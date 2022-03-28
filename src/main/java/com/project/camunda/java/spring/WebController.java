f6f9794d972e5947f9bf9ec473ce8d21c6f8a06d
adityalp <adityalishadi03@gmail.com>
2022-03-28 17:26:06 +0700

    ADD: import to eclipse project & add front page

--- src/main/java/com/project/camunda/java/spring/WebController.java
+++ src/main/java/com/project/camunda/java/spring/WebController.java
@@ -5,6 +5,16 @@
  */
 package com.project.camunda.java.spring;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,11 +27,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
     
-    private final String message = "Abdul Salam";
+    private final String message = "Abdul";
     
-    @GetMapping("/hello")
+    @GetMapping("/main")
     public String homePage(Model model) {
-        model.addAttribute("message", message);
+		model.addAttribute("dates", getDate());
+        
         return "index";
     }
+    
+    public List<String> getDate() {
+    	SimpleDateFormat first_date = new SimpleDateFormat("dd.MM");
+		SimpleDateFormat last_date = new SimpleDateFormat("dd.MM.yyyy");
+			
+		List<String> allDates = new ArrayList<>();
+		Calendar cal = Calendar.getInstance();
+		cal.get(Calendar.MONTH);
+		cal.set(Calendar.DATE, 1);
+				
+		for (int i = 1; i <= 12; i++) {
+		    String first_month = first_date.format(cal.getTime());
+		    cal.set(Calendar.DATE, cal.getActualMaximum(Calendar.DATE));
+		    String last_month = last_date.format(cal.getTime());
+		    		    
+		    allDates.add(first_month+"-"+last_month);
+		    cal.add(Calendar.MONTH, +1);
+		    cal.set(Calendar.DATE, 1);
+		}
+		
+		return allDates;
+    }
+    
 }
